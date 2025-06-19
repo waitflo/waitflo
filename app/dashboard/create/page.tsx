4d21660fd2bf582e34790e084898dbcc23fd67fd
teledrop <victoradoghe916@gmail.com>
2025-06-19 16:10:46 +0100

    chore: remove React Bricks and all content from create flow page; add @grapesjs/studio-sdk for future integration

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,11 +1,5 @@
 "use client"
 
-import dynamic from "next/dynamic";
-
-const CreateFlowEditor = dynamic(() => import("./CreateFlowEditor"), {
-  ssr: false,
-});
-
 export default function CreateFlowPage() {
-  return <CreateFlowEditor />;
+  return null;
 }
